fcce5b7da5d7a603076cf19da2d6e03821f52c8b
Jacob Dmn <jacob.demmane@email.com>
2022-03-16 23:08:17 +0400

    final touches

--- components/general_components/StepCard.tsx
+++ components/general_components/StepCard.tsx
@@ -1,13 +1,30 @@
 import React from 'react'
-import Image from 'next/image';
+import Image from 'next/image'
 
-const StepCard = ({data}:any) => {
+const StepCard = ({ data }: any) => {
   return (
-    <div className='text-center max-w-xs relative basis-1/3 green_line'>
-      <Image src={data.imageUrl} width={120} height={120} alt="how buddytree works" className='object-cover' />
-      <span className='absolute py-1 left-34 top-22 w-12 h-12 bg-Green rounded-full text-white text-center text-2xl font-semibold border-2 border-white'>{data.id}</span>
-      <h1 className='text-center text-xl text-DarkBlue mt-8 md:whitespace-nowrap'>{data.title}</h1>
-      <p className='text-center text-Grey mt-5 text-base font-light'>{data.desc}</p>
+    <div className="green_line relative max-w-xs basis-1/3 text-center">
+      <Image
+        src={data.imageUrl}
+        width={120}
+        height={120}
+        alt="how buddytree works"
+        className="object-cover"
+      />
+      <span className="absolute left-34 top-22 h-12 w-12 rounded-full border-2 border-white bg-Green py-1 text-center text-2xl font-semibold text-white">
+        {data.id}
+      </span>
+      <h1 className="mt-8 text-center text-xl font-bold text-DarkBlue md:whitespace-nowrap">
+        {data.title}
+      </h1>
+      <p
+        className={
+          `mt-5 text-center text-base font-light text-Grey` +
+          `${data.bold && 'font-bold'}`
+        }
+      >
+        {data.desc}
+      </p>
       {/* <div className='absolute top-28 -right-28 h-0.5 w-44 bg-Green last-of-type:hidden'></div> */}
     </div>
   )
--- utils/steps.ts
+++ utils/steps.ts
@@ -1,16 +1,33 @@
-import first from "../assets/images/how_bodytree_works/1.png";
-import second from "../assets/images/how_bodytree_works/2.png";
-import third from "../assets/images/how_bodytree_works/3.png";
+import first from '../assets/images/how_bodytree_works/1.png'
+import second from '../assets/images/how_bodytree_works/2.png'
+import third from '../assets/images/how_bodytree_works/3.png'
 
-interface Card{
-    id:number,
-    title:string,
-    desc:string,
-    imageUrl:any
+interface Card {
+  id: number
+  title: string
+  desc: string
+  imageUrl: any
+  bold?: boolean
 }
 
-export const steps:Array<Card>=[
-    {id:1,title:"Create your free account",desc:"Members show their faces in their profile picture.No creeps. Join your first live hangout for free." ,imageUrl:first},
-    {id:2,title:"Join your first live hangout",desc:"Discover over 50 topics or create your own, filled with interesting, meaningful, and unexpected conversations.",imageUrl:second},
-    {id:3,title:"Have great conversations with people",desc:"Buddytree has an amazing community of people. We can't wait for you to make lasting friendships and build your chosen family.",imageUrl:third}
+export const steps: Array<Card> = [
+  {
+    id: 1,
+    title: 'Create your free account',
+    desc: 'Members show their faces in their profile picture.No creeps. Join your first live hangout for free.',
+    imageUrl: first,
+  },
+  {
+    id: 2,
+    title: 'Join your first live hangout',
+    desc: 'Discover over 50 topics or create your own, filled with interesting, meaningful, and unexpected conversations.',
+    imageUrl: second,
+    bold: true,
+  },
+  {
+    id: 3,
+    title: 'Have great conversations with people',
+    desc: "Buddytree has an amazing community of people. We can't wait for you to make lasting friendships and build your chosen family.",
+    imageUrl: third,
+  },
 ]
